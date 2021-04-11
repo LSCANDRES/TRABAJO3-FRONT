afc3d8e2d2a1fee7ec57e817b108a8d67e2e3681
LSCANDRES <andreslunasalas@gmail.com>
2021-04-11 19:13:11 -0300

    Un poco de codigo js

---
+++ pruebas/app.js
@@ -0,0 +1,34 @@
+ /*    const carta= document.querySelector(".card-group") /* una ves que funcione esto a dentro remplazo los id para mostrar diferente inbformacion  */
+ /*    carta.innerHTML= '<h2>PRODdasdasdCTOS</h2>' */
+
+
+
+
+
+function llamarestereo(){
+    document.getElementById("#estereo1")
+    window.open("../pruebas/prueba.html")
+
+};
+
+function llamarparlante(){
+    document.getElementById("#parlantes")
+    window.open("../pruebas/prueba.html")
+};
+ 
+
+
+const cambiopantalla =document.querySelector("#consolas")
+
+
+cambiopantalla.addEventListener('click', (abrir) => {
+    window.open("../pruebas/prueba.html")
+    cambiotarjeta.innerHTML='<h2>18000$</h2>'
+
+
+    const cambiotarjeta=document.querySelector("#precio")
+    cambiotarjeta.innerHTML='<h2>18000$</h2>'
+
+    
+    
+})
